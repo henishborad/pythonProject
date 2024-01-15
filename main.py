48b56a3a34bf69aaa2401d0aebbff0e99bb6021b
Henish Borad <henishborad@gmail.com>
2024-01-14 16:17:02 -0800

    update

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-def factorial(num=int(input("Enter num: "))):
-    fact = 1
-    if num < 0:
-        print("Invalid")
-    if num == 0:
-        print("Factorial of 0 is 1.")
-    if num > 0:
-        for i in range(1, num + 1):
-            fact = fact * i
-        print(f"Factorial of {num} is {fact}.")
-    return fact
-
-
-factorial()
+# def factorial(num=int(input("Enter num: "))):
+#     fact = 1
+#     if num < 0:
+#         print("Invalid")
+#     if num == 0:
+#         print("Factorial of 0 is 1.")
+#     if num > 0:
+#         for i in range(1, num + 1):
+#             fact = fact * i
+#         print(f"Factorial of {num} is {fact}.")
+#     return fact
+#
+#
+# factorial()
