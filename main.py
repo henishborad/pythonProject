5a4fa598cb8ca01767c1f9d3a0909f1257fcf354
Henish Borad <henishborad@gmail.com>
2024-01-15 21:10:01 -0800

    update

---
+++ main.py
@@ -0,0 +1,19 @@
+# if __name__ == '__main__':
+#     n = int(input())
+#
+#     my_list = list(range(n))
+#     for i in my_list:
+#         print(i * i)
+
+
+first = 10
+second = 20
+print(first)
+print(second)
+
+first = first + second
+second = first - second
+first = first - second
+
+print(first)
+print(second)
---
+++ movie.py
@@ -0,0 +1,4 @@
+class Movie():
+    def __init__(self, title, actor):
+        self.title = title
+        self.actor = actor
